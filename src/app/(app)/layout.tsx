73d03d13d4e36c21e85749fc43a0ec0919b6421a
Arjay Q. Ramiso <arjayramiso@gmail.com>
2026-02-21 12:44:03 +0800

    Added initial setup for login and refactored the layout

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,29 @@
+import { AppSidebar } from '@/components/app-sidebar'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import { TooltipProvider } from '@/components/ui/tooltip'
+import { SiteHeader } from '@/components/site-header'
+
+export default function AppLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <TooltipProvider>
+      <SidebarProvider
+        style={
+          {
+            '--sidebar-width': 'calc(var(--spacing) * 72)',
+            '--header-height': 'calc(var(--spacing) * 12)'
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <SiteHeader />
+          {children}
+        </SidebarInset>
+      </SidebarProvider>
+    </TooltipProvider>
+  )
+}
